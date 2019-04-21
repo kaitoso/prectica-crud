4b4ac7dbcc22a0f15a6523cbc5c8a13f849d7c6c
Carlos Valenzuela <carlosr.valenzuela.r@gmail.com>
2019-04-20 22:39:49 -0400

    primer componente agregado

---
+++ src/app/Componente-uno/uno.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'uno',
+    template: '<h1>Prueba primer componente</h1>'
+})
+export class ClaseUno {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClaseUno } from './Componente-uno/uno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClaseUno
   ],
   imports: [
     BrowserModule,
